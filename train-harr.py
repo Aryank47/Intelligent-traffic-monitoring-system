6897ced56b9b9c8446a25661845d3cdb8c30b5f6
AbhishekSahu87 <m23csa504@iitj.ac.in>
2025-04-14 21:11:56 +0530

    Update train-harr.py
    changed size

--- train-harr.py
+++ train-harr.py
@@ -1,6 +1,6 @@
 !pip install opencv-python
 
-opencv_createsamples -info /content/data/positives.txt -num 1200 -w 24 -h 24 -vec vec_file.vec
+opencv_createsamples -info /content/data/positives.txt -num 1200 -w 64 -h 64 -vec vec_file.vec
 
 opencv_traincascade -data /content/data -vec /content/vec_file.vec -bg /content/data/negatives.txt -numPos 900 -numNeg 500 -numStages 10 -w 24 -h 24
 
